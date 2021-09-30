d3d04b40dbadec267c852fb467711939ebc851c6
inseon park <inseon.park@power-gen.co.kr>
2021-09-30 17:58:47 +0900

    05_

--- ts-react-redux-tutorial/src/modules/counter.ts
+++ ts-react-redux-tutorial/src/modules/counter.ts
@@ -1,64 +1,33 @@
-// 액션 타입을 선언합니다
-// 뒤에 as const 를 붙여줌으로써 나주엥 액션 객체를 만들게 action.type의 값을 추론하는 과정에서
-// action.type 이 string으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 갑으로 추론 되도록
-
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
+import { deprecated, ActionType, createReducer } from "typesafe-actions";
+const { createStandardAction } = deprecated;
 
 // 액션 생성함수를 선언합니다
-export const increase = () => ({
-  type: INCREASE,
-});
+export const increase = createStandardAction("counter/INCREASE")();
+export const decrease = createStandardAction("counter/DECREASE")();
+export const increaseBy = createStandardAction("counter/INCREASE_BY")<number>(); // payload 타입을 Generics 로 설정해주세요.
 
-export const decrease = () => ({
-  type: DECREASE,
-});
+// 액션 객체 타입 준비
+const actions = { increase, decrease, increaseBy }; // 모든 액션 생성함수들을 actions 객체에 넣습니다
+type CounterAction = ActionType<typeof actions>; // ActionType 를 사용하여 모든 액션 객체들의 타입을 준비해줄 수 있습니다
 
-export const increaseby = (diff: number) => ({
-  type: INCREASE_BY,
-  // 액션에 보가적으로 필요한 값을 payload 라는 이름으로 통일합니다
-  // 이는 FSA라는 규칙인데
-  // 이규칙을 적용하면 액션들이 모두 비슷한 구조로 이루어져있게 되어 추후 다룰 때도 편하고
-  // 읽기 쉽고, 액션 구조를 일반화함으로써 액션에 관련된 라이브러리를 사용 할 수 있게 해줍니다.
-  // 다만, 무조건 꼭 따를 필요는 없습니다.
-  payload: diff,
-});
-
-// 모든 액션 객체들에 대한 타입을 준비해줍니다.
-// ReturnType<typeof _____> 는 특정 함수의 반환값을 추론해줍니다.
-// 상단부에서 액션타입을 선언 할 때 as const를 하지 않으면 이 부분이 제대로 작동하지 않습니다.
-type CounterAction =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseby>;
-
-// 이 리덕스 모듈에서 관리 할 상태의 타입을 선언합니다.
+// 이 리덕스 모듈에서 관리 할 상태의 타입을 선언합니다
 type CounterState = {
   count: number;
 };
 
+// 초기상태를 선언합니다.
 const initialState: CounterState = {
   count: 0,
 };
 
-// 리듀서를 작성합니다.
-// 리듀서에서는 state 와 함수의 반환값이 일치하도록 작성하세요.
-// 액션에서는 우리가 방금 만든 CounterAction 을 타입으로 설정합니다.
-function counter(
-  state: CounterState = initialState,
-  action: CounterAction
-): CounterState {
-  switch (action.type) {
-    case INCREASE: // case 라고 입력하고 Ctrl+ space를 누르면 어떤 종류의 action.type들이 있는지 확인 가능
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return { count: state.count + action.payload };
-    default:
-      return state;
-  }
-}
+// 리듀서를 만듭니다
+// createReducer 는 리듀서를 쉽게 만들 수 있게 해주는 함수입니다.
+// Generics로 리듀서에서 관리할 상태, 그리고 리듀서에서 처리 할 모든 액션 객체들의 타입을 넣어야합니다
+const counter = createReducer<CounterState, CounterAction>(initialState)
+  .handleAction(increase, (state) => ({ count: state.count + 1 }))
+  .handleAction(decrease, (state) => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload,
+  }));
 
 export default counter;
